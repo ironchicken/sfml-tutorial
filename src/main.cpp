86df6c352a1c1b210cc4e37febe60d3e34218fdf
Richard Lewis <richard@rjlewis.me.uk>
2022-09-15 00:25:45 +0100

    Animate the circle

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,18 @@ int main(int argc, char* argv[]) {
     sf::RenderWindow window{ sf::VideoMode(640, 480), "SFML Tutorial" };
     sf::CircleShape shape{ 100.f };
     shape.setFillColor(sf::Color::Green);
+    shape.setRadius(5.0);
 
     const int frameRate { 30 };
     const sf::Time targetDelayMS = sf::milliseconds(1000 / frameRate);
 
     sf::Event event;
     sf::Clock clock;
+
+    int circleX = 640 / 2;
+    int circleY = 480 / 2;
+    float velocityX = 140.0;
+    float velocityY = 140.0;
 
     while (window.isOpen()) {
         while (window.pollEvent(event)) {
@@ -22,6 +28,24 @@ int main(int argc, char* argv[]) {
         if (elapsed < targetDelayMS) {
             sf::sleep(targetDelayMS - elapsed);
         }
+
+        if (circleX >= 640 && velocityX > 0.0) {
+            velocityX = -140.0;
+        }
+        if (circleX <= 0 && velocityX < 0.0) {
+            velocityX = 140.0;
+        }
+        if (circleY >= 480 && velocityY > 0.0) {
+            velocityY = -140.0;
+        }
+        if (circleY <= 0 && velocityY < 0.0) {
+            velocityY = 140.0;
+        }
+
+        circleX += elapsed.asSeconds() * velocityX;
+        circleY += elapsed.asSeconds() * velocityY;
+
+        shape.setPosition(circleX, circleY);
 
         window.clear();
         window.draw(shape);
